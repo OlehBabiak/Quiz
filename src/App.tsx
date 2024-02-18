ff67da2859474504fa74a0f5f619e2aee153109a
Oleh_Babiak <oleh_babiak@epam.com>
2024-02-18 12:28:10 +0200

    maim pages created, icons added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { useTranslation } from "react-i18next"
+import './shared/i18n/i18n.js'
+import i18next from "i18next";
+import { LANGUAGES } from "./shared/constants/i18n/constants.js";
 
 
 function App() {
 
+  const {t} = useTranslation();
+
+
   return (
     <>
       <div>
-        Hello
+        {t("Select Language")}
       </div>
+      <div>{i18next.language}</div>
+      <button disabled={i18next.language === LANGUAGES.EN} onClick={() => i18next.changeLanguage(LANGUAGES.EN)}>English</button>
+      <button disabled={i18next.language === LANGUAGES.FR} onClick={() => i18next.changeLanguage(LANGUAGES.FR)}>French</button>
+      <button disabled={i18next.language === LANGUAGES.DE} onClick={() => i18next.changeLanguage(LANGUAGES.DE)}>German</button>
+      <button disabled={i18next.language === LANGUAGES.SP} onClick={() => i18next.changeLanguage(LANGUAGES.SP)}>Spanish</button>
     </>
   )
 }
---
+++ src/shared/constants/i18n/constants.ts
@@ -0,0 +1,6 @@
+export const enum LANGUAGES {
+    EN = 'en',
+    DE = 'de',
+    SP = 'sp',
+    FR = 'fr'
+  }
